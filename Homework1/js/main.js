b8f47cf412619b2e02bd5d80323d85effedbdb1e
Anastasia Ustinova <72691594+Cvikken@users.noreply.github.com>
2020-10-18 14:03:16 +0300

    Update main.js
    внесена информация с урока, поправила класс ProductItem чтобы не было undefined при выводе на страницу

--- Homework1/js/main.js
+++ Homework1/js/main.js
@@ -1,22 +1,93 @@
-const products = [
-  {id: 1, title: 'Notebook', price: 20000},
-  {id: 2, title: 'Mouse', price: 1500},
-  {id: 3, title: 'Keyboard', price: 5000},
-  {id: 4, title: 'Gamepad', price: 4500},
-];
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-const renderProduct = (title, price, img = 'http://unsplash.it/150/150?random&gravity=center') => {
-  return `<div class="product-item">
-            <img class="img-item" src="${img}" alt="">
-            <h3>${title}</h3>
-            <p>${price} &pound;</p>
-            <button class="by-btn">Добавить в корзину</button>
-          </div>`;
+// Переделать в ДЗ не использовать fetch а Promise
+let getRequest = (url, cb) => {
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      if (xhr.status !== 200) {
+        console.log('Error');
+      } else {
+        cb(xhr.responseText);
+      }
+    }
+  };
+  xhr.send();
 };
 
-const renderProducts = (list) => {
-  const productList = list.map(item => renderProduct(item.title, item.price));
-  document.querySelector('.products').innerHTML = productList.join('');
-};
+// –--------------------------------
 
-renderProducts(products);
+class ProductList {
+  #goods;
+
+  constructor(container = '.products') {
+    this.container = container;
+    this.#goods = [];
+    this._allProducts = [];
+
+    // this._fetchGoods();
+    this.#getProducts().then((data) => {
+      this.#goods = [...data];
+      // this.#goods = Array.from(data);
+      this.#render();
+    });
+
+    console.log(this.sum());
+  }
+
+  // _fetchGoods() {
+  //   getRequest(`${API}/catalogData.json`, (data) => {
+  //     console.log(data);
+  //     this.#goods = JSON.parse(data);
+  //     this.#render();
+  //     console.log(this.#goods);
+  //   });
+  // }
+
+  #getProducts() {
+    return fetch(`${API}/catalogData.json`)
+        .then(response => response.json())
+        .catch((error) => {
+          console.log(error);
+        });
+  }
+
+  sum() {
+    return this.#goods.reduce((sum, { price }) => sum + price, 0);
+  }
+
+  #render() {
+    const block = document.querySelector(this.container);
+
+    for (let product of this.#goods) {
+      const productObject = new ProductItem(product);
+
+      this._allProducts.push(productObject);
+
+      block.insertAdjacentHTML('beforeend', productObject.getGoodHTML());
+    }
+  }
+}
+
+class ProductItem {
+  constructor(product, img = 'http://unsplash.it/150/150?random&gravity=center') {
+    this.title = product.product_name;
+    this.price = product.price;
+    this.id = product.id_product;
+    this.img = img;
+  }
+
+  getGoodHTML() {
+    return `<div class="product-item" data-id="${this.id}">
+              <img src="${this.img}" alt="Some img">
+              <div class="desc">
+                  <h3>${this.title}</h3>
+                  <p>${this.price} \u20bd</p>
+                  <button class="buy-btn">Купить</button>
+              </div>
+            </div>`;
+  }
+}
+
+const list = new ProductList();
